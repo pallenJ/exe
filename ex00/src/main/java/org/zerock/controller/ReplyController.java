62c5810843790e147db6a011c5705e8b34bca392
박준모 <@DESKTOP-UCEAMBJ>
2019-05-21 15:56:14 +0900

    수정, 삭제, 목록기능 추가

--- ex00/src/main/java/org/zerock/controller/ReplyController.java
+++ ex00/src/main/java/org/zerock/controller/ReplyController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
@@ -46,5 +48,25 @@ public class ReplyController {
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
 	
 	}
+	
+	@GetMapping(value = "/{rno}", produces = {
+	MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE		
+	})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno){
+		return new ResponseEntity<>(service.get(rno),HttpStatus.OK);
+		}
+	
+	@DeleteMapping(value="/{rno}",produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+	return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+			: new ResponseEntity<>("success", HttpStatus.INTERNAL_SERVER_ERROR);	
+	}
 
+	@RequestMapping(method = {RequestMethod.PUT,RequestMethod.PATCH}, value = "/{rno}",
+	consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno")Long rno){
+		return service.modify(vo)==1? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>("success", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 }
