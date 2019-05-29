27a4e2b265c18b682e99ad4b198577ce2df49e8a
박준모 <@DESKTOP-UCEAMBJ>
2019-05-29 13:24:53 +0900

    로그인 시큐리티 적용

---
+++ ex01/src/main/java/org/zerock/domain/CustomUser.java
@@ -0,0 +1,36 @@
+package org.zerock.domain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import lombok.Getter;
+
+@Getter
+public class CustomUser extends User{
+	
+	public CustomUser(String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CustomUser(MemberVO vo) {
+		super(vo.getUserid(), vo.getUserpw(), vo.getAuthList().stream().map(
+				auth -> new SimpleGrantedAuthority(auth.getAuth()))
+				.collect(Collectors.toList())	);
+		this.member = vo;
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+	private MemberVO member;
+
+	
+	
+	
+}
